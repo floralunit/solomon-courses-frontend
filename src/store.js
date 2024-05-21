ba1243055e4d34a0c8aaefa02a94f248ca53be7d
floralunit <anya71034@gmail.com>
2024-05-22 02:16:46 +0300

    127623637274 исправление долбаного redux

--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
-import { createStore } from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
-import { applyMiddleware, createAsyncThunk } from '@reduxjs/toolkit'
-import {thunk} from "redux-thunk";
+import { createStore,applyMiddleware } from "redux";
+import { composeWithDevTools } from '@redux-devtools/extension';
+
+import {thunk} from 'redux-thunk';
+
 import rootReducer from "./reducers";
 const middleware = [thunk];
 const store = createStore(
     rootReducer,
+    composeWithDevTools(
     applyMiddleware(...middleware)
+    )
 );
 export default store;
